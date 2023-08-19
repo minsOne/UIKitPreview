90b848a23743623edb6ebecf9ab9271b09f2e530
minsOne <jungminahn.minsone@gmail.com>
2023-08-19 21:38:43 +0900

    SwiftUI의 View가 onAppear 되었을 때 printA11y를 호출하도록 변경

--- Sources/UIKitPreview/A11y/Preview+A11y.swift
+++ Sources/UIKitPreview/A11y/Preview+A11y.swift
@@ -2,22 +2,25 @@ import Foundation
 import SwiftUI
 
 public extension ViewPreview {
-    func printA11y(border: Bool = true) -> Self {
-        view.printA11y(border: border)
-        return self
+    func printA11y(border: Bool = true) -> some SwiftUI.View {
+        onAppear {
+            view.printA11y(border: border)
+        }
     }
 }
 
 public extension ViewControllerPreview {
-    func printA11y(border: Bool = true) -> Self {
-        viewController.view.printA11y(border: border)
-        return self
+    func printA11y(border: Bool = true) -> some View {
+        onAppear {
+            viewController.view.printA11y(border: border)
+        }
     }
 }
 
 public extension NavigationControllerPreview {
-    func printA11y(border: Bool = true) -> Self {
-        navigationController.topViewController?.view.printA11y(border: border)
-        return self
+    func printA11y(border: Bool = true) -> some View {
+        onAppear {
+            navigationController.topViewController?.view.printA11y(border: border)
+        }
     }
 }
